b0cd4a2fe2e8a10f8b222f305ac6082602b366a8
vckgec <vcserver.net@gmail.com>
2018-02-16 17:39:28 +0530

    mess models fix

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 app_name = 'gallery'
 urlpatterns = [
-    url(r'^$', views.Youtube, name='gallery'),
-    #url(r'^youtube/$', views.Youtube, name='youtube'),
+    url(r'^$', views.Home, name='gallery'),
+    url(r'^youtube/$', views.Youtube, name='youtube'),
     ]
