25923a483959f051de753cf02eaf6cd781661cb3
vckgec <vcserver.net@gmail.com>
2018-06-14 17:40:05 +0530

    static deleted

--- static/library/esearch.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    document.getElementById("loader").style.marginLeft = ($('#results').width() / 2 - $('#loader').width() / 2) + "px";
-    document.getElementById("loader").style.marginTop = $('nav').height() + "px";
-    $('#results').load("/library/libgen/ #myDiv", function () {
-        document.getElementById("myDiv").style.display = "block";
-        document.getElementById("loader").style.display = "none";
-    });
-});
