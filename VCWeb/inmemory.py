b72a03397ef02ff56b0e2af547dbfd76a092f9f1
Bibhas Mondal <bibhasmondal96@gmail.com>
2018-09-21 17:35:11 +0530

    123

--- VCWeb/inmemory.py
+++ VCWeb/inmemory.py
@@ -2,5 +2,5 @@ from mess.updates import presenceCreate
 from asgiref.inmemory import ChannelLayer
 class MyChannelLayer(ChannelLayer):
     def __init__(self):
-        # presenceCreate()
+        presenceCreate()
         super(MyChannelLayer,self).__init__(capacity=10000000)
